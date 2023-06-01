836be79907e5eed1d7596e8b7c454e58b5199460
Dipankar <dipankarcode@gmail.com>
2023-06-02 03:36:13 +0530

    Database added

--- index.js
+++ index.js
@@ -13,49 +13,51 @@ bot.command('quit', async (ctx) => {
 	await ctx.leaveChat();
 });
 
+
+// Tea🍵
+
 const DATA_BASE = {
 	Monday: {
 		'/morning': 'Pav Bhaji + Tea🍵',
-		'/afternoon':
-			'Baingan Bharta , Dal-Fry, Plain-Rice, Curd, Salad, Chapati and Fruits',
-		'/evening': 'Poha',
-		'/night': 'Chicken',
+		'/afternoon': '🍆 Baingan-Bharta, 🐄 Curd',
+		'/evening': 'Masala-Pav 🍔 +  Tea 🍵',
+		'/night': 'Matar-Panner, Papad, Rice-kher  ',
 	},
 	Tuesday: {
-		'/morning': 'Samosa',
-		'/afternoon': 'Puri',
-		'/evening': 'Poha',
-		'/night': 'Chicken',
+		'/morning': `Poha / Black Chana/ Dalia , Coffee ☕`,
+		'/afternoon': `Rajma-Masala , Crud`,
+		'/evening': `Samosa + Tea🍵`,
+		'/night': `Puri/Chapati  , Custard🍮`,
 	},
 	Wednesday: {
-		'/morning': 'Samosa',
-		'/afternoon': 'Puri',
-		'/evening': 'Poha',
-		'/night': 'Chicken',
+		'/morning': `Idli + ( Sambhar / Coconut Chutney )[ Alt  ] `,
+		'/afternoon': `Veg Biryani, Papad , Raita`,
+		'/evening': `Vada Pav, Tea`,
+		'/night': `Chicken`,
 	},
 	Thursday: {
-		'/morning': 'Samosa',
-		'/afternoon': 'Puri',
-		'/evening': 'Poha',
-		'/night': 'Chicken',
+		'/morning': `Puri Bhaji + Tea🍵`,
+		'/afternoon': `Rajma-masala + Curd`,
+		'/evening':`'Poha , Coffee`,
+		'/night': `Aloo Gobhi / Kofta Curry`,
 	},
 	Friday: {
-		'/morning': 'Samosa',
-		'/afternoon': 'Puri',
-		'/evening': 'Poha',
-		'/night': 'Chicken',
+		'/morning': `Bread Butter - Veg Cutlet / Boiled Egg + Tea🍵`,
+		'/afternoon': `ALoo Matar, Curd`,
+		'/evening': `Bread Pakoda + Tea🍵`,
+		'/night': `[ 🍽️ Paid 💰 ] -- Loki / Soyabean Sabzi`,
 	},
 	Saturday: {
-		'/morning': 'Samosa',
-		'/afternoon': 'Puri',
-		'/evening': 'Poha',
-		'/night': 'Chicken',
+		'/morning': `Aloo Paratha , Curd + Tea🍵`,
+		'/afternoon': `Chole Bhatoore / Jeera Rice, Boondi Raita`,
+		'/evening': `Bhel + Coffe☕`,
+		'/night': `Soyabeen`,
 	},
 	Sunday: {
-		'/morning': 'Samosa',
-		'/afternoon': 'Puri',
-		'/evening': 'Poha',
-		'/night': 'Chicken',
+		'/morning': `Onion-Tomato Uttapam 🔥 + Sambhar + Tea🍵`,
+		'/afternoon': `Chana Masala , Jeera rice`,
+		'/evening': `Cream Roll + Tea🍵`,
+		'/night': `[ 🍽️ Paid 💰 ] -- Aloo Palak , Mix dal`,
 	},
 };
 
