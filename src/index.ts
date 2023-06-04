444f36dcc91fed36672e2c3efc6426f60918642c
Dipankar <dipankarcode@gmail.com>
2023-06-04 19:59:14 +0530

    Testing out Photos - Successs : ) .. Yeeee !! 🤩

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,25 @@ import { weekDay } from './Utils/DayTime.js';
 dotenv.config();
 const bot = new Telegraf(process.env.BOT_TOKEN || 'noKey');
 
+
+
 bot.on(message('text'), async (ctx) => {
 	const selected_time: string = ctx.update.message.text;
 	const food = (await DATA_BASE[weekDay][selected_time]) ||'Please Select the Options from List';
 	await ctx.reply(food);
+
+	// await ctx.reply((ctx.chat.id).toString()) ;
+
+	// Testing .. 
+	await ctx.replyWithPhoto('https://images.unsplash.com/photo-1596797038530-2c107229654b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80') ;
+	// ctx.telegram.getChat() ;
+
 });
 
 bot.on('callback_query', async (ctx) => {
 	// Using context shortcut
 	await ctx.answerCbQuery("Yo....");
+
 });
 
 bot.launch();
