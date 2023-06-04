47745b96f25d129c9e92b59134211388adebc571
Dipankar <dipankarcode@gmail.com>
2023-06-04 23:53:00 +0530

    Cleaning up the mess

--- src/index.ts
+++ src/index.ts
@@ -3,45 +3,17 @@ import { message } from 'telegraf/filters';
 import dotenv from 'dotenv';
 import { DATA_BASE } from './Data/messMenu.js';
 import { weekDay } from './Utils/DayTime.js';
-import * as fs from 'fs';
-import path from 'path';
-
 
 dotenv.config();
+
 const bot = new Telegraf(process.env.BOT_TOKEN || 'noKey');
 
-
-bot.command('upload', (ctx) => {
-
-	let imageFilePath = "C:/CODE_C/Projects/AIT_MESS_Telegram_Bot/src/food_american.webp" ;
-	const imageStream = fs.createReadStream(imageFilePath);
-	ctx.replyWithPhoto({ source: imageStream });
-});
-
 bot.on(message('text'), async (ctx) => {
+
 	const selected_time: string = ctx.update.message.text;
-	const food =		(await DATA_BASE[weekDay][selected_time]) ||	'Please Select the Options from List';
+	const food = (await DATA_BASE[weekDay][selected_time]) ||	'Please Select the Options from List';
 	await ctx.reply(food);
-	// await ctx.reply((ctx.chat.id).toString()) ;
-
-	console.log(ctx)
-
-	// Testing ..
-	await ctx.replyWithPhoto(
-		'https://images.unsplash.com/photo-1596797038530-2c107229654b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'
-	);
-
-
-
-	// ctx.telegram.getChat() ;
 });
-
-
-
-bot.on(message('photo'), async (ctx) => {
-	console.log(ctx);
-});
-
 
 bot.on('callback_query', async (ctx) => {
 	// Using context shortcut
